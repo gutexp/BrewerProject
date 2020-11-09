61fd0e0ffb413ba80a64b3841459c4d15bde849d
gutexp <gutexp@hotmail.com>
2020-11-09 02:48:32 -0300

    message

---
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -0,0 +1,39 @@
+package com.algaworks.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.algaworks.brewer.model.Cerveja;
+
+@Controller
+public class CervejasController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+
+	@RequestMapping("/cervejas/novo")
+	public String novo(Cerveja cerveja) {
+		
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return novo(cerveja);
+		}
+		
+		// Salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		System.out.println(">>> sku: " + cerveja.getSku());
+		return "redirect:/cervejas/novo";
+	}
+	
+}
