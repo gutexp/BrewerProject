60dfff9cb706de9f586332852cab377f782fe0de
gutexp <gutexp@hotmail.com>
2020-11-10 16:55:46 -0300

    changes of 10.2
    -Adds a custom bean validation to SKU

---
+++ src/main/java/com/algaworks/brewer/validation/SKU.java
@@ -0,0 +1,28 @@
+package com.algaworks.brewer.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.OverridesAttribute;
+import javax.validation.Payload;
+import javax.validation.constraints.Pattern;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@Pattern(regexp = "([a-zA-Z]{2}\\d{4})?")
+public @interface SKU {
+	
+	//é interessante que modularizemos essa mensagem para que nos poupe tempo e deixe mais legível quando precisarmos de validação em outros modelos
+	
+	@OverridesAttribute(constraint = Pattern.class, name = "message")
+	String message() default "SKU deve seguir o padrão XX9999";
+	
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+	
+	
+}
