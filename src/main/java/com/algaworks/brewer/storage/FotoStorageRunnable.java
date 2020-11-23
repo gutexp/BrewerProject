36bef1afc815d512442edc99cb8911b01e258847
gutexp <gutexp@hotmail.com>
2020-11-23 19:15:39 -0300

    14.5 changes
    x

---
+++ src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -0,0 +1,25 @@
+package com.algaworks.brewer.storage;
+
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FotoStorageRunnable implements Runnable {
+
+	private MultipartFile[] files;
+	private DeferredResult<String> resultado;
+	
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<String> resultado) {
+		this.files = files;
+		this.resultado = resultado;
+	}
+	
+	
+	@Override
+	public void run() {
+		System.out.println(">>> files" + files[0].getSize());
+		
+		resultado.setResult("OK! Foto recebida");
+
+	}
+
+}
