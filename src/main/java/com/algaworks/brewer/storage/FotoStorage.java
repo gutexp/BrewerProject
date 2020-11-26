0edc96ff7e285654b127ad9b893664a18f3e6203
gutexp <gutexp@hotmail.com>
2020-11-26 20:50:27 -0300

    changes from 15.3
    -Adde a mock when the beer don't have any photo;
    -Redo the dimension of each images to show that on the thumbs when we search for beers;

--- src/main/java/com/algaworks/brewer/storage/FotoStorage.java
+++ src/main/java/com/algaworks/brewer/storage/FotoStorage.java
@@ -5,7 +5,11 @@ import org.springframework.web.multipart.MultipartFile;
 public interface FotoStorage {
 
 	public String salvarTemporariamente(MultipartFile[] files);
-	
+
 	public byte[] recuperarFotoTemporaria(String nome);
+
+	public void salvar(String foto);
+
+	public byte[] recuperar(String foto);
 	
 }
