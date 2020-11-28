6266c6ec8d90bb6dad968208c3a844bb29d1f657
gutexp <gutexp@hotmail.com>
2020-11-28 00:45:03 -0300

    Adds changes from 15.15 and 15.16
    x

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -1,12 +1,16 @@
 package com.algaworks.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +18,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.repository.Estilos;
+import com.algaworks.brewer.repository.filter.EstiloFilter;
 import com.algaworks.brewer.service.CadastroEstiloService;
 import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
 
@@ -24,6 +31,9 @@ public class EstilosController {
 
 	@Autowired
 	private CadastroEstiloService cadastroEstiloService;
+	
+	@Autowired
+	private Estilos estilos;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Estilo estilo) {
@@ -38,11 +48,10 @@ public class EstilosController {
 		
 		try {
 			cadastroEstiloService.salvar(estilo);
-		}catch(NomeEstiloJaCadastradoException e) {
+		} catch (NomeEstiloJaCadastradoException e) {
 			result.rejectValue("nome", e.getMessage(), e.getMessage());
 			return novo(estilo);
 		}
-		
 		
 		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
 		return new ModelAndView("redirect:/estilos/novo");
@@ -50,14 +59,23 @@ public class EstilosController {
 	
 	@RequestMapping(method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
 	public @ResponseBody ResponseEntity<?> salvar(@RequestBody @Valid Estilo estilo, BindingResult result) {	//o tipo do método tem que ser responsebody para poder estar retornando o responseEntity isso tudo faz parte de se utilizar ajax no código
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
 		}
 		
-
-		cadastroEstiloService.salvar(estilo);	//com o controllerAdviceExceptionHandler conseguimos tratar a excessão que pode acontecer aqui
-		
+		estilo = cadastroEstiloService.salvar(estilo);		//com o controllerAdviceExceptionHandler conseguimos tratar a excessão que pode acontecer aqui
 		return ResponseEntity.ok(estilo);
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar(EstiloFilter estiloFilter, BindingResult result
+			, @PageableDefault(size = 2) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("estilo/PesquisaEstilos");
+		
+		PageWrapper<Estilo> paginaWrapper = new PageWrapper<>(estilos.filtrar(estiloFilter, pageable)
+				, httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+		return mv;
 	}
 	
 }
