0d23a85012e06f71d6c107d2c32a2a802be214dd
gutexp <gutexp@hotmail.com>
2020-12-11 00:09:51 -0300

    Changes from 19.9
    Renables the csrf and threats the caused bugs of it

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -75,6 +75,23 @@ Brewer.MaskDate = (function(){
 
 }());
 
+Brewer.Security = (function(){
+
+	function Security(){
+		this.token = $('input[name=_csrf]').val();
+		this.header = $('input[name=_csrf_header]').val();
+	}
+
+	Security.prototype.enable = function(){
+		$(document).ajaxSend(function(event, jqxhr, settings){
+			jqxhr.setRequestHeader(this.header, this.token);
+		}.bind(this));
+	}
+
+	return Security;
+
+}());
+
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
 	maskMoney.enable();
@@ -87,5 +104,8 @@ $(function() {
 
 	var maskDate = new Brewer.MaskDate();
 	maskDate.enable();
+
+	var security = new Brewer.Security();
+	security.enable();
 	
 });
