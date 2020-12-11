de2cec6fdaec41b3bb447edaecb997ce45d12b0f
gutexp <gutexp@hotmail.com>
2020-12-11 01:35:21 -0300

    Changes from 19.13
    Now the page shows the name of the logged user.

--- src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
+++ src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -6,17 +6,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import com.algaworks.brewer.model.Usuario;
-import com.algaworks.brewer.repository.Usuarios;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.repository.Usuarios;
 
 @Service
 public class AppUserDetailsService implements UserDetailsService {
@@ -31,7 +30,7 @@ public class AppUserDetailsService implements UserDetailsService {
         Usuario usuario = usuarioOptional
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
 
-        return new User(usuario.getEmail(), usuario.getSenha(), getPermissoes(usuario));
+        return new UsuarioSistema(usuario , getPermissoes(usuario));
     }
 
     private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
