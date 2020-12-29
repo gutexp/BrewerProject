f6e192f0868cb33cadff19eed698c54fd8697894
gutexp <gutexp@hotmail.com>
2020-12-28 23:23:41 -0300

    changes from 23.7
    -Adds new test cases;
    -Beers are now shown when selected;

--- src/main/java/com/algaworks/brewer/controller/VendasController.java
+++ src/main/java/com/algaworks/brewer/controller/VendasController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/vendas")
@@ -28,11 +29,13 @@ public class VendasController {
 	}
 
 	@PostMapping("/item")
-	public @ResponseBody String adicionarItem(Long codigoCerveja){
+	public ModelAndView adicionarItem(Long codigoCerveja){
 		Cerveja cerveja = cervejas.findOne(codigoCerveja);
 		tabelaItensVenda.adicionarItem(cerveja, 1);
-		System.out.println(">>> Total de itens: " + tabelaItensVenda.total());
-		return "Item adicionado!";
+		ModelAndView mv = new ModelAndView("venda/TabelaItensVenda");
+		mv.addObject("itens", tabelaItensVenda.getItens());
+
+		return mv;
 
 	}
 
